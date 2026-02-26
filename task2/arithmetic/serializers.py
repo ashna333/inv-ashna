5a847b8f5d127aecb976a785dd9dcb202cbe4b01
ashnamkhalid <ashnamkhalid333@gmail.com>
2026-02-26 09:46:55 +0530

    Added confirm_password

--- task2/arithmetic/serializers.py
+++ task2/arithmetic/serializers.py
@@ -2,11 +2,19 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password= serializers.CharField(write_only=True)
+    confirm_password= serializers.CharField(write_only=True)
 
     class Meta:
         model=User
-        fields=['username','email','password']
+        fields=['username','email','password','confirm_password']
+        extra_kwargs={
+            'password': {'write_only': True}
+        }
+    
+    def validat(self,data):
+        if data['password']!=data['confirm_password']:
+            raise serializers.ValidationError("passwords do no match")
+        return data
 
 
     def create(self,validated_data):
